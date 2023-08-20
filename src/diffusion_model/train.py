e94d9198689c089a3f99e0844dbed9f2511df626
Bijay Gaudel <57655873+gaudelbijay@users.noreply.github.com>
2023-08-20 12:55:35 -0400

    Update train.py

--- src/diffusion_model/train.py
+++ src/diffusion_model/train.py
@@ -8,21 +8,21 @@ model = Unet(
 
 diffusion = GaussianDiffusion(
     model,
-    image_size = 128,
+    image_size = 448,
     timesteps = 1000,           # number of steps
-    sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+    sampling_timesteps = 50    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
 trainer = Trainer(
     diffusion,
-    '../data/raw/',
-    train_batch_size = 32,
+    '../data/airsim/raw',
+    train_batch_size = 8,
     train_lr = 8e-5,
-    train_num_steps = 100,         # total training steps
+    train_num_steps = 100000,         # total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
     amp = True,                       # turn on mixed precision
-    calculate_fid = False              # whether to calculate fid during training
+    calculate_fid = True              # whether to calculate fid during training
 )
 
-trainer.load(100)
+trainer.train()
